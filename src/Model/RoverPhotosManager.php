f0ce384abe2543cc4aefad991f0b63f82dd0bb5f
APauquin <alexandrep123@yahoo.com>
2023-05-11 21:18:23 +0200

    had to ignore grump or error in controller

--- src/Model/RoverPhotosManager.php
+++ src/Model/RoverPhotosManager.php
@@ -11,8 +11,7 @@ class RoverPhotosManager
         $client = HttpClient::create();
         $response = $client->request(
             'GET',
-            'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/
-            photos?sol=1000&page=3&api_key=aaw0O1BzH8D2RyndzzS5BnBJr210NetO19gzLkMt'
+            'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=3&api_key=aaw0O1BzH8D2RyndzzS5BnBJr210NetO19gzLkMt'
         );
         return $response->toArray();
     }
